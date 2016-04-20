44b9ec7e7986e9de590c640b9ffe71f5a6b2ce22
L1J Smitty <l1jsmitty@gmail.com>
2016-04-19 20:28:36 -0400

    Remove hard-coded path for lineage directory

--- Launcher/LauncherForm.cs
+++ Launcher/LauncherForm.cs
@@ -45,7 +45,7 @@ namespace Launcher
 
         public LauncherForm()
         {
-            var appLocation = @"C:\Program Files (x86)\Lineage Resurrection";//Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var appLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var associatedLaunchers = Helpers.GetAssociatedLaunchers(appLocation);
             
             if (!Helpers.LauncherInLineageDirectory(appLocation))
