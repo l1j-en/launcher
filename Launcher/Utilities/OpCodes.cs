15d104c143b9267177d38bbb643706a8178370f7
L1J Smitty <l1jsmitty@gmail.com>
2017-03-10 19:25:51 -0500

    start of adding old proxy back

---
+++ Launcher/Utilities/OpCodes.cs
@@ -0,0 +1,18 @@
+﻿namespace Launcher.Utilities
+{
+    public class OpCodes
+    {
+        public enum ClientOpCodes : byte
+        {
+            Attack = 5,
+            LoginPacket = 57,
+            ClientVersion = 127
+        }
+
+        public enum ServerOpCodes : byte
+        {
+            InitPacket = 161,
+            AttackPacket = 35
+        }
+    }
+}
