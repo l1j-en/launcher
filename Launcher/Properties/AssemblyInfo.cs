29e98f93445cb7e4c0442373a294384383c6f4e1
L1J Smitty <l1jsmitty@gmail.com>
2018-08-18 22:45:22 -0400

    updated path and version #'s

--- Launcher/Properties/AssemblyInfo.cs
+++ Launcher/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("B000DDA8-CD27-492C-8A06-094152516DFA")]
+[assembly: Guid("5ec48b7d-2f3e-4e98-ba96-2c0688be5a43")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.7.6.0")]
-[assembly: AssemblyFileVersion("2.7.6.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
